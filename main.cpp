3ec1ecd5eca4396a9b29371bcf623a0b5ba5e3e7
ymozer <yusufmetinozer@gmail.com>
2023-03-14 16:34:53 +0300

    implemented external libraries to workflow. triangle algorithm is ok for now.

--- main.cpp
+++ main.cpp
@@ -13,101 +13,92 @@
 
 
 // function to draw a line using Bresenham's algorithm
-void drawLine(int x1, int y1, int x2, int y2, int width, int height, unsigned char* data) {
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
+void drawLine(glm::vec2 a, glm::vec2 b, glm::vec2 image_size, unsigned char* data) {
+    int dx = abs(b.x - a.x);
+    int dy = abs(b.y - a.y);
+    int sx = (a.x < b.x) ? 1 : -1;
+    int sy = (a.y < b.y) ? 1 : -1;
     int err = dx - dy;
 
     while (true) {
         //int index = (y1 * width + x1) * 3; // 3 bytes per pixel
-        int index = ((height - 1 - y1) * width + x1) * 3; // 3 bytes per pixel
-
-        data[index] = 255; // red channel
-        data[index + 1] = 255; // green channel
-        data[index + 2] = 255; // blue channel
-
-        if (x1 == x2 && y1 == y2) {
-            break;
+        int index = ((image_size.y - 1 - a.y) * image_size.x + a.x) * 3; // 3 bytes per pixel
+        if (index >= 0 && index + 2 < image_size.x * image_size.y * 3) { // check if index is within bounds
+            data[index] = 255; // red channel
+            data[index + 1] = 255; // green channel
+            data[index + 2] = 255; // blue channel
         }
         int e2 = 2 * err;
-        if (e2 > -dy) {
+
+        if (a.y == b.y) {
             err -= dy;
-            x1 += sx;
+            a.x += sx;
         }
-        if (e2 < dx) {
-            err += dx;
-            y1 += sy;
+        else {
+
+            if (a.x== b.x && a.y == b.y) {
+                break;
+            }
+
+            if (e2 > -dy) {
+                err -= dy;
+                a.x+= sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                a.y += sy;
+            }
         }
     }
 }
 
-void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int width, int height, unsigned char* data) {
+void fillTriangle(glm::vec2 v0, glm::vec2 v1, glm::vec2 v2, glm::vec2 image_size, unsigned char* data) {
     // sort the vertices by y-coordinate ascending so v1 is the top-most vertice
-    if (y1 > y2) {
-		std::swap(y1, y2);
-		std::swap(x1, x2);
-	}
-    if (y2 > y3) {
-		std::swap(y2, y3);
-		std::swap(x2, x3);
-	}
-    if (y1 > y2) {
-		std::swap(y1, y2);
-		std::swap(x1, x2);
-	}
-
-	// compute the slopes
-	float m12 = (float)(x2 - x1) / (float)(y2 - y1);
-	float m23 = (float)(x3 - x2) / (float)(y3 - y2);
-	float m13 = (float)(x3 - x1) / (float)(y3 - y1);
-
-	// draw the first half of the triangle
-    for (int y = y1; y <= y2; ++y) {
-		int xStart = x1 + (y - y1) * m12;
-		int xEnd = x1 + (y - y1) * m13;
-		drawLine(xStart, y, xEnd, y, width, height, data);
-	}
-
-	// draw the second half of the triangle
-    for (int y = y2; y <= y3; ++y) {
-		int xStart = x1 + (y - y1) * m13;
-		int xEnd = x2 + (y - y2) * m23;
-		drawLine(xStart, y, xEnd, y, width, height, data);
-	}
-    
+    if (v0.y > v1.y) std::swap(v0, v1);
+    if (v0.y > v2.y) std::swap(v0, v2);
+    if (v1.y > v2.y) std::swap(v1, v2);
+    int total_height = v2.y - v0.y;
+    for (int i = 0; i < total_height; i++) {
+        bool second_half = i > v1.y - v0.y || v1.y == v0.y;
+        int half_height = second_half ? v2.y - v1.y : v1.y - v0.y;
+        float a = (float)i / total_height;
+        float b = (float)(i - (second_half ? v1.y - v0.y : 0)) / half_height;
+        glm::vec2 A = v0 + (v2 - v0) * a;
+        glm::vec2 B = second_half ? v1 + (v2 - v1) * b: v0 + (v1 - v0) * b;
+        if (A.x > B.x) std::swap(A,B);
+        for (int j = A.x; j <= B.x; j++) {
+           // set pixel using stb_image_write
+            int index = (int)(j + (v0.y + i) * image_size[1]) * 3;
+            data[index] = 255;
+            data[index+1] = 255;
+            data[index+2] = 255;
+        }
+    }
 }
 
-glm::mat4 transform(glm::vec2 const& Orientation, glm::vec3 const& Translate, glm::vec3 const& Up)
-{
-    glm::mat4 Proj = glm::perspective(glm::radians(45.f), 1.33f, 0.1f, 10.f);
-    glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.f), Translate);
-    glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Orientation.y, Up);
-    glm::mat4 View = glm::rotate(ViewRotateX, Orientation.x, Up);
-    glm::mat4 Model = glm::mat4(1.0f);
-    return Proj * View * Model;
-}
+
+
 
 int main() {
     const int width = 512;
     const int height = 512;
-    unsigned char* data = new unsigned char[width * height * 3];
+    glm::vec2 img_size = glm::vec2(width, height);
+    unsigned char* data = new unsigned char[ img_size.x * img_size.y * 3];
     for (int i = 0; i < width * height * 3; ++i) {
         data[i] = 0;
     }
 
-    drawLine(50, 10, 500, 400, width, height, data);
+    std::vector<glm::vec2> vertices = {
+        glm::vec2(5,5),
+        glm::vec2(50,50),
+        glm::vec2(100,5)
+    };
 
-    fillTriangle(400, 234,30,30,259,450,width,height, data);
-
-    // write the pixel data to a TGA file
+    fillTriangle(vertices[0], vertices[1], vertices[2], img_size, data);
+    stbi_flip_vertically_on_write(true);
     stbi_write_tga("triangle.tga", width, height, 3, data);
     std::cout << "Line saved to triangle.tga" << std::endl;
-    std::cout <<  "_________" << std::endl;
 
-    glm::mat4 transform_mat=transform(glm::vec2(1,2),glm::vec3(12,12,-79),glm::vec3(99,45,0) );
-    std::cout << glm::to_string(transform_mat) << std::endl;
     delete[] data;
     return 0;
 }
